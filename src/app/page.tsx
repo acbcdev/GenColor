a004ef0a616628cc2800948cb61eb5fcc02ff8e7
acbc <acbc.dev@gmail.com>
2025-04-25 20:08:37 -0500

    Standardize code formatting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
-export default function Home () {
+export default function Home() {
   return (
     <main className='w-full h-screen flex justify-center items-center'>
-      <h1 className='text-4xl font-bold'>
-        Hello world!!
-      </h1>
+      <h1 className='text-4xl font-bold'>Hello world!!</h1>
     </main>
   )
 }
