3dba498d23faa218da59e3fb9445a998e6856ffd
acbc <acbc.dev@gmail.com>
2025-04-28 19:05:45 -0500

    Use Tailwind's rounded-t-lg class

--- src/components/features/generator/colorPalette.tsx
+++ src/components/features/generator/colorPalette.tsx
@@ -2,20 +2,18 @@ import { Card, CardHeader, CardContent } from '@/components/ui/card'
 
 const data = ['#1E1E1E', '#3A3A3A', '#D4D4D4', '#FF9E6D', '#69D2E7']
 
-export default function ColorPalette () {
+export default function ColorPalette() {
   return (
     <div className='flex justify-between gap-6'>
       {data.map((item: string) => (
         <Card key={item} className='w-full py-0 pb-3 gap-0'>
           <CardHeader className='p-0 m-0'>
             <div
-              className='w-full h-[100px] rounded-t-[.5rem]'
+              className='w-full h-[100px] rounded-t-lg'
               style={{ backgroundColor: item }}
             />
           </CardHeader>
-          <CardContent className=''>
-            {item}
-          </CardContent>
+          <CardContent>{item}</CardContent>
         </Card>
       ))}
     </div>
