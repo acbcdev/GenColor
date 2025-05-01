457b8ca2a5f205dcd8ac92c67b67bf23631e70a7
acbc <acbc.dev@gmail.com>
2025-04-30 19:01:50 -0500

    fix: update Prettier and ESLint configurations for consistency in quotes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
-import { dirname } from 'path'
-import { fileURLToPath } from 'url'
-import { FlatCompat } from '@eslint/eslintrc'
-import eslintConfigPrettier from 'eslint-config-prettier/flat'
+import { dirname } from "path"
+import { fileURLToPath } from "url"
+import { FlatCompat } from "@eslint/eslintrc"
+import eslintConfigPrettier from "eslint-config-prettier/flat"
 const filename = fileURLToPath(import.meta.url)
 const dir = dirname(filename)
 
@@ -10,34 +10,34 @@ const compat = new FlatCompat({
 })
 
 const eslintConfig = [
-  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
   eslintConfigPrettier,
   {
     rules: {
-      'comma-dangle': ['error', 'never'],
-      semi: ['error', 'never'],
-      'react-hooks/exhaustive-deps': 'off',
-      'func-call-spacing': ['error', 'never'],
-      eqeqeq: ['error', 'always'],
-      'no-var': 'error',
-      'prefer-const': 'error',
-      'object-curly-spacing': ['error', 'always'],
-      'array-bracket-spacing': ['error', 'never'],
-      'no-console': ['warn', { allow: ['warn', 'error'] }],
-      'no-loop-func': 'error',
-      'no-eval': 'error',
-      'prefer-template': 'error',
-      'no-dupe-keys': 'error',
-      'no-duplicate-case': 'error',
-      'no-multi-spaces': 'error',
-      'no-trailing-spaces': 'error',
-      'space-infix-ops': 'error',
-      'keyword-spacing': 'error',
-      'comma-spacing': ['error', { before: false, after: true }],
-      quotes: ['error', 'single'],
-      'jsx-quotes': ['error', 'prefer-single'],
-      '@typescript-eslint/no-empty-object-type': 'off',
-      '@typescript-eslint/no-explicit-any': 'off'
+      "comma-dangle": ["error", "never"],
+      semi: ["error", "never"],
+      "react-hooks/exhaustive-deps": "off",
+      "func-call-spacing": ["error", "never"],
+      eqeqeq: ["error", "always"],
+      "no-var": "error",
+      "prefer-const": "error",
+      "object-curly-spacing": ["error", "always"],
+      "array-bracket-spacing": ["error", "never"],
+      "no-console": ["warn", { allow: ["warn", "error"] }],
+      "no-loop-func": "error",
+      "no-eval": "error",
+      "prefer-template": "error",
+      "no-dupe-keys": "error",
+      "no-duplicate-case": "error",
+      "no-multi-spaces": "error",
+      "no-trailing-spaces": "error",
+      "space-infix-ops": "error",
+      "keyword-spacing": "error",
+      "comma-spacing": ["error", { before: false, after: true }],
+      quotes: ["error", "double"],
+      "jsx-quotes": ["error", "double-double"],
+      "@typescript-eslint/no-empty-object-type": "off",
+      "@typescript-eslint/no-explicit-any": "off"
     }
   }
 ]
